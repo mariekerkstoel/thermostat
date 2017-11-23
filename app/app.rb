8a48fd95ccb86a41fb9657c18b0b85e3384e5f9e
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-23 17:19:41 +0000

    requires db-setup in app.rb

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,9 @@
 require 'sinatra/base'
 
+require_relative 'models/db-setup'
+# require_relative 'models/city'
+# require_relative 'models/user'
+
 class App < Sinatra::Base
   get '/' do
     erb(:index)
