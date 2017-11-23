4bc9022aeb293088614499664881fc0a864066f4
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-23 17:23:39 +0000

    fixes bugs in databases

--- app/models/db-setup.rb
+++ app/models/db-setup.rb
@@ -4,6 +4,6 @@ require 'dm-postgres-adapter'
 require_relative 'city'
 require_relative 'user'
 
-DataMapper.setup("postgres://localhost/thermostat_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, "postgres://localhost/thermostat_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
