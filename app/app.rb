428ca96a775fe86482ec5355e832cb4c326d4d6a
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-23 18:09:16 +0000

    adds routes to server

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,4 @@
-ENV['RACK_ENV'] = 'development' 
+ENV['RACK_ENV'] = 'development'
 
 require 'sinatra/base'
 
@@ -7,7 +7,36 @@ require_relative 'models/db-setup'
 # require_relative 'models/user'
 
 class App < Sinatra::Base
+  enable :sessions
+
+  helpers do
+    def current_user
+      User.first(session[:user_id])
+    end
+  end
+
   get '/' do
+    unless session[:user_id]
+      user = User.create(temp: 20, city: City.first_or_create(name: 'London'))
+      session[:user_id] = user.id
+    end
     erb(:index)
+  end
+
+  get '/user' do
+    content_type :json
+    "{\"temp\":#{current_user.temp},\"city\":\"#{current_user.city.name}\"}"
+  end
+
+  post '/temperature' do
+    current_user.temp = params[:temp].to_i
+    current_user.save
+    status_code(204)
+  end
+
+  post '/city' do
+    current_user.city = City.first_or_create(name: params[:city])
+    current_user.save
+    status_code(204)
   end
 end
